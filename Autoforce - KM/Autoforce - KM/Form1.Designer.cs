b2147c8b1ca84cb03317b03b257a378ea72b8208
Suellen Oliveira Prog <98441845+suellen-oliveira-prog@users.noreply.github.com>
2022-04-05 09:37:35 -0300

    Add files via upload

---
+++ Autoforce - KM/Autoforce - KM/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Autoforce___KM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Teste com Campo em Branco";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 34);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Teste com Caracteres";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(16, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(163, 30);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Teste com mais de 9 dígitos";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(197, 70);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(163, 30);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Todos os testes";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 143);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(344, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Insira o Link para teste:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 198);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Teste campo Km";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Autoforce - KM/Autoforce - KM/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+using NUnit.Framework;
+namespace Autoforce___KM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(textBox1.Text);
+            //Teste do campo em branco
+            driver.FindElement(By.CssSelector(".form__control-select__placeholder")).Click();
+            driver.FindElement(By.Id("react-select-2-option-0")).Click();
+            driver.FindElement(By.CssSelector(".btn-primary")).Click();
+            Assert.That(driver.FindElement(By.Id("bouncer-error_used_vehicle[km]")).Text, Is.EqualTo("Por favor, preencha esse campo"));
+            //Validação do campo.
+            driver.FindElement(By.Id("used_vehicle_km")).Click();
+            driver.FindElement(By.Id("used_vehicle_km")).SendKeys("123456789-123");
+            driver.FindElement(By.CssSelector(".form__control-select__value-container")).Click();
+            driver.FindElement(By.Id("react-select-2-option-1")).Click();
+            driver.FindElement(By.Id("used_vehicle_km")).Click();
+            {
+                string value = driver.FindElement(By.Id("used_vehicle_km")).GetAttribute("value");
+                Assert.That(value, Is.EqualTo("123.456.789 Km"));
+            }
+            driver.Quit();
+            MessageBox.Show("Todos os testes foram efetuados com sucesso");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(textBox1.Text);
+            driver.FindElement(By.Id("used_vehicle_km")).Click();
+            driver.FindElement(By.Id("used_vehicle_km")).SendKeys("123456789-123");
+            driver.FindElement(By.CssSelector(".form__control-select__value-container")).Click();
+            driver.FindElement(By.Id("react-select-2-option-1")).Click();
+            driver.FindElement(By.Id("used_vehicle_km")).Click();
+            {
+                string value = driver.FindElement(By.Id("used_vehicle_km")).GetAttribute("value");
+                Assert.That(value, Is.EqualTo("123.456.789 Km"));
+            }
+            driver.Quit();
+            MessageBox.Show("Campo limita corretamente a 9 números.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            IWebDriver driver = new FirefoxDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(textBox1.Text);
+            driver.FindElement(By.Id("used_vehicle_km")).Click();
+            driver.FindElement(By.Id("used_vehicle_km")).SendKeys("abcdefg teste");
+            driver.FindElement(By.CssSelector(".form__control-select__value-container")).Click();
+            driver.FindElement(By.Id("react-select-2-option-1")).Click();
+            driver.FindElement(By.Id("used_vehicle_km")).Click();
+            {
+                string value = driver.FindElement(By.Id("used_vehicle_km")).GetAttribute("value");
+                Assert.That(driver.FindElement(By.Id("bouncer-error_used_vehicle[km]")).Text, Is.EqualTo("Por favor, preencha esse campo"));
+            }
+            driver.Quit();
+            MessageBox.Show("Campo permanece em branco, mesmo após tentar inserir caracteres.");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            IWebDriver driver = new FirefoxDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(textBox1.Text);
+            driver.FindElement(By.CssSelector(".form__control-select__placeholder")).Click();
+            driver.FindElement(By.Id("react-select-2-option-0")).Click();
+            driver.FindElement(By.CssSelector(".btn-primary")).Click();
+            Assert.That(driver.FindElement(By.Id("bouncer-error_used_vehicle[km]")).Text, Is.EqualTo("Por favor, preencha esse campo"));
+            driver.Quit();
+            MessageBox.Show("A mensagem de campo em branco está correta!");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
